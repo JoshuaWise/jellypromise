4717fad4c474f0fcfb4919c5da5efe0a020b3ccf
Joshua Wise <josh@joshuawise.ninja>
2016-04-03 05:11:43 -0400

    redid code style

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-'use strict';
-
-module.exports = require('./lib')
+'use strict'
+module.exports = require('./lib/core.js')
+require('./lib/es6-extensions.js')
+require('./lib/node-extensions.js')
+require('./lib/utility.js')
+require('./lib/rejection-tracking.js')
