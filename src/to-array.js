82d886a062a7583215a8baf34db730ce0edabff8
Joshua Wise <josh@joshuawise.ninja>
2016-04-07 16:12:15 -0400

    made to-array work for old browsers

--- src/to-array.js
+++ src/to-array.js
@@ -1,9 +1,18 @@
 'use strict'
-module.exports = require('./iterator-symbol')
-	? require('./to-array-modern')
-	: function (iterable) {
-		if (typeof iterable.length === 'number') {
-			return Array.prototype.slice.call(iterable)
-		}
-		throw new TypeError('Expected argument to be an iterable or array-like object.')
+var iterator = require('./iterator-symbol')
+
+module.exports = function (iterable) {
+	if (typeof iterable.length === 'number') {
+		return Array.prototype.slice.call(iterable)
 	}
+	if (iterator && typeof iterable[iterator] === 'function') {
+		var arr = []
+		var it = iterable[iterator]()
+		var item
+		while (!(item = it.next()).done) {
+			arr.push(item.value)
+		}
+		return arr
+	}
+	throw new TypeError('Expected argument to be an iterable or array-like object.')
+}
