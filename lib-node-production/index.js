ff4ea18f26d6018213b98b0d9bf4651785da1ba7
Joshua Wise <josh@joshuawise.ninja>
2016-04-07 15:23:36 -0400

    postpublish unbuild

--- lib-node-production/index.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-module.exports = require('./core')
-require('./utilities')
-require('./node-extensions')
--- lib-node/iterator-symbol.js
+++
@@ -1,4 +0,0 @@
-'use strict'
-module.exports = typeof Symbol === 'function' && typeof Symbol.iterator === 'symbol'
-	? Symbol.iterator
-	: undefined
