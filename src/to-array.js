709573753011f6a4f51f728e8056e5753a68ef16
Joshua Wise <josh@joshuawise.ninja>
2016-04-07 15:48:23 -0400

    added iterable fallback

--- src/to-array.js
+++ src/to-array.js
@@ -1,14 +1,9 @@
 'use strict'
-var iterator = require('./iterator-symbol')
-
-var toArray = module.exports = function (iterable) {
-	if (typeof iterable.length === 'number') {
-		return Array.prototype.slice.call(iterable)
+module.exports = require('./iterator-symbol')
+	? require('./to-array-modern')
+	: function (iterable) {
+		if (typeof iterable.length === 'number') {
+			return Array.prototype.slice.call(iterable)
+		}
+		throw new TypeError('Expected argument to be an iterable or array-like object.')
 	}
-	if (iterator && typeof iterable[iterator] === 'function') {
-		var arr = []
-		for (var value of iterable) {arr.push(value)}
-		return arr
-	}
-	throw new TypeError('Expected argument to be an iterable or array-like object.')
-}
