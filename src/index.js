65409a1e50bb32b255dddb6107ba4bd6af27108e
Joshua Wise <josh@joshuawise.ninja>
2016-04-09 02:58:19 -0400

    renamed more files

--- src/index.js
+++ src/index.js
@@ -5,21 +5,24 @@ if (process.env.NODE_ENV === 'production') {
 	module.exports = require('../production')
 } else {
 	module.exports = require('./promise')
-	require('./utilities')
-	// require('./aggregate-methods')
-	require('./node-extensions')
+	require('./methods-private')
+	require('./methods-utility')
+	// require('./methods-aggregate')
+	require('./methods-node')
 	require('./long-stack-traces')
 }
 // @[/]
 // @[production node]
 module.exports = require('./promise')
-require('./utilities')
-// require('./aggregate-methods')
-require('./node-extensions')
+require('./methods-private')
+require('./methods-utility')
+// require('./methods-aggregate')
+require('./methods-node')
 // @[/]
 // @[browser]
 module.exports = require('./promise')
-require('./utilities')
-// require('./aggregate-methods')
+require('./methods-private')
+require('./methods-utility')
+// require('./methods-aggregate')
 require('./long-stack-traces') // @[/development]
 // @[/]
