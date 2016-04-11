e52eb75cd5e5f9de4118ebbe3ac8cca65cb03662
Joshua Wise <josh@joshuawise.ninja>
2016-04-11 04:22:08 -0400

    updated todo

--- test-memory-leak/basic.js
+++ test-memory-leak/basic.js
@@ -3,16 +3,17 @@ var assert = require('assert')
 var Promise = require('../')
 
 module.exports = function (callback) {
+	var baseCount = 1000 // 100000
 	var i = 0
 	var sampleA
 	var sampleB
 	(function next() {
 		return new Promise(function (resolve) {
-			if (i++ === 100000) {
+			if (i++ === baseCount) {
 				global.gc()
 				sampleA = process.memoryUsage().heapUsed
 			}
-			if (i > 100000 * 10) {
+			if (i > baseCount * 10) {
 				global.gc()
 				sampleB = process.memoryUsage().heapUsed
 				console.log('Memory usage at start:\t', sampleA)
