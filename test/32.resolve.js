40ff9ec89258e162ea9024f8b3520c9078403fa8
Joshua Wise <josh@joshuawise.ninja>
2016-05-18 15:27:53 -0400

    added tests for resolve and reject

--- test/32.resolve.js
+++ test/32.resolve.js
@@ -1,5 +1,61 @@
 'use strict'
 require('../tools/describe')('Promise.resolve', function (Promise, expect) {
-	it('should be tested')
+	it('should be fulfilled with undefined (implicit)', function () {
+		return expect(Promise.resolve()).to.eventually.equal(undefined)
+	})
+	it('should be fulfilled with undefined (explicit)', function () {
+		return expect(Promise.resolve(undefined)).to.eventually.equal(undefined)
+	})
+	it('should be fulfilled with null', function () {
+		return expect(Promise.resolve(null)).to.eventually.equal(null)
+	})
+	it('should be fulfilled with true', function () {
+		return expect(Promise.resolve(true)).to.eventually.equal(true)
+	})
+	it('should be fulfilled with false', function () {
+		return expect(Promise.resolve(false)).to.eventually.equal(false)
+	})
+	it('should be fulfilled with NaN', function () {
+		return expect(Promise.resolve(NaN)).to.eventually.be.NaN
+	})
+	it('should be fulfilled with -Infinity', function () {
+		return expect(Promise.resolve(-Infinity)).to.eventually.equal(-Infinity)
+	})
+	it('should be fulfilled with 12345', function () {
+		return expect(Promise.resolve(12345)).to.eventually.equal(12345)
+	})
+	it('should be fulfilled with \'foobar\'', function () {
+		return expect(Promise.resolve("foobar")).to.eventually.equal("foobar")
+	})
+	it('should be fulfilled with \'\'', function () {
+		return expect(Promise.resolve('')).to.eventually.equal('')
+	})
+	if (typeof Symbol === 'function') {
+		it('should be fulfilled with a symbol', function () {
+			var sym = Symbol('foo')
+			return expect(Promise.resolve(sym)).to.eventually.equal(sym)
+		})
+	}
+	it('should be fulfilled with an object', function () {
+		var obj = {foo: 'bar'}
+		return expect(Promise.resolve(obj)).to.eventually.equal(obj)
+	})
+	it('should be fulfilled with an array', function () {
+		var arr = ['foo', 'bar']
+		return expect(Promise.resolve(arr)).to.eventually.equal(arr)
+	})
+	it('should be fulfilled with a regular exporession object', function () {
+		var re = /foobar/i
+		return expect(Promise.resolve(re)).to.eventually.equal(re)
+	})
+	it('should be fulfilled immediately', function () {
+		expect(Promise.resolve({}).inspect().state).to.equal('fulfilled')
+	})
+	it('should return the given trusted promise', function () {
+		var p = Promise.resolve(555)
+		var res = Promise.resolve(p)
+		expect(p).to.equal(res)
+		return expect(res).to.eventually.equal(555)
+	})
 	it('should test being given foreign thenables')
 })
--- test/33.reject.js
+++ test/33.reject.js
@@ -1,5 +1,103 @@
 'use strict'
 require('../tools/describe')('Promise.reject', function (Promise, expect) {
-	it('should be tested')
-	it('should test being given foreign thenables')
+	it('should be rejected with undefined (implicit)', function () {
+		return Promise.reject().then(function () {throw new Error('The promise should have bee rejected.')},
+			function (reason) {
+				expect(reason).to.equal(undefined)
+			})
+	})
+	it('should be rejected with undefined (explicit)', function () {
+		return Promise.reject(undefined).then(function () {throw new Error('The promise should have bee rejected.')},
+			function (reason) {
+				expect(reason).to.equal(undefined)
+			})
+	})
+	it('should be rejected with null', function () {
+		return Promise.reject(null).then(function () {throw new Error('The promise should have bee rejected.')},
+			function (reason) {
+				expect(reason).to.equal(null)
+			})
+	})
+	it('should be rejected with true', function () {
+		return Promise.reject(true).then(function () {throw new Error('The promise should have bee rejected.')},
+			function (reason) {
+				expect(reason).to.equal(true)
+			})
+	})
+	it('should be rejected with false', function () {
+		return Promise.reject(false).then(function () {throw new Error('The promise should have bee rejected.')},
+			function (reason) {
+				expect(reason).to.equal(false)
+			})
+	})
+	it('should be rejected with -Infinity', function () {
+		return Promise.reject(NaN).then(function () {throw new Error('The promise should have bee rejected.')},
+			function (reason) {
+				expect(reason).to.be.NaN
+			})
+	})
+	it('should be rejected with -Infinity', function () {
+		return Promise.reject(-Infinity).then(function () {throw new Error('The promise should have bee rejected.')},
+			function (reason) {
+				expect(reason).to.equal(-Infinity)
+			})
+	})
+	it('should be rejected with 12345', function () {
+		return Promise.reject(12345).then(function () {throw new Error('The promise should have bee rejected.')},
+			function (reason) {
+				expect(reason).to.equal(12345)
+			})
+	})
+	it('should be rejected with \'foobar\'', function () {
+		return Promise.reject('foobar').then(function () {throw new Error('The promise should have bee rejected.')},
+			function (reason) {
+				expect(reason).to.equal('foobar')
+			})
+	})
+	it('should be rejected with \'\'', function () {
+		return Promise.reject('').then(function () {throw new Error('The promise should have bee rejected.')},
+			function (reason) {
+				expect(reason).to.equal('')
+			})
+	})
+	if (typeof Symbol === 'function') {
+		it('should be rejected with a symbol', function () {
+			var sym = Symbol('foo')
+			return Promise.reject(sym).then(function () {throw new Error('The promise should have bee rejected.')},
+				function (reason) {
+					expect(reason).to.equal(sym)
+				})
+		})
+	}
+	it('should be rejected with an object', function () {
+		var obj = {foo: 'bar'}
+		return Promise.reject(obj).then(function () {throw new Error('The promise should have bee rejected.')},
+			function (reason) {
+				expect(reason).to.equal(obj)
+			})
+	})
+	it('should be rejected with an array', function () {
+		var arr = ['foo', 'bar']
+		return Promise.reject(arr).then(function () {throw new Error('The promise should have bee rejected.')},
+			function (reason) {
+				expect(reason).to.equal(arr)
+			})
+	})
+	it('should be rejected with a regular exporession object', function () {
+		var re = /foobar/i
+		return Promise.reject(re).then(function () {throw new Error('The promise should have bee rejected.')},
+			function (reason) {
+				expect(reason).to.equal(re)
+			})
+	})
+	it('should be rejected immediately', function () {
+		expect(Promise.reject({}).catchLater().inspect().state).to.equal('rejected')
+	})
+	it('should be rejected with a promise', function () {
+		var p = Promise.resolve(555)
+		return Promise.reject(p).then(function () {throw new Error('The promise should have bee rejected.')},
+			function (reason) {
+				expect(reason).to.equal(p)
+			})
+	})
 })
