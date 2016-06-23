1373d1c02fd1dc2cd1dcdaad38dbb4589f2c33ec
Joshua Wise <josh@joshuawise.ninja>
2016-06-22 23:21:42 -0400

    added pending filter tests

---
+++ test/70.filter.js
@@ -0,0 +1,14 @@
+'use strict'
+// var ArrayTester = require('../tools/test/array-tester')
+// var shallowEquals = require('../tools/test/shallow-equals')
+// var makeIterable = require('../tools/test/make-iterable')
+// var testNonIterables = require('../tools/test/test-non-iterables')
+require('../tools/test/describe')('.filter', function (Promise, expect) {
+	// var arrayTester = new ArrayTester(Promise)
+	// function expectToMatch(input, source) {
+	// 	return expect(Promise.all(input)).to.eventually.satisfy(shallowEquals(source))
+	// }
+	
+	it('should do Promise.all-like tests with a consistent callback function')
+	it('should test an array of values (as done in bluebird), against different types of callback functions')
+})
