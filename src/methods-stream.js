69b234f9022ba29a130965679efaf64b6f98dbf6
Joshua Wise <joshuathomaswise@gmail.com>
2016-09-30 06:37:30 -0400

    cleaned up, added notes

--- src/methods-stream.js
+++ src/methods-stream.js
@@ -14,6 +14,19 @@ function PromiseStream(source) {
 	this._process = null
 	this._flush = _flushQueue
 	// Could signal desiredSize as (highWaterMark - processing - queue._length)
+	// When cleanly CLOSED, should use ._end() on piped streams
+	// Should propagate errors to piped streams
+	// If no teeing is implemented, errors should also propagate backwards
+	// Should be able to access a closedPromise (that rejects with _reason)
+	// Provide reading methods that don't pipe to a new stream; possibilities:
+	// - drain() -> emitter
+	// - merge() -> promise of array
+	// - reduce() -> promise of value
+	// - read() -> promise of {value, done}, for single-item pulling
+	// Don't forget other piping methods:
+	// - filter()
+	// - forEach()
+	// - sort()
 	
 	if (source === INTERNAL) {
 		this._removeListeners = NOOP
@@ -35,8 +48,12 @@ PromiseStream.from = function (iterable) {
 	stream._switchToIteratorMode(iterable)
 	return stream
 }
-PromiseStream.prototype.abort = function () {
-	this._error(new TypeError('This stream was aborted.'))
+PromiseStream.prototype.abort = function (reason) {
+	if (reason == null) {
+		reason = new TypeError('This stream was aborted.')
+	}
+	this._error(reason)
+	// intentionally returns undefined
 }
 PromiseStream.prototype.map = function (concurrency, handler) {
 	if (this._state === $STREAM_CLOSED) {
@@ -67,7 +84,7 @@ PromiseStream.prototype._write = function (data) {
 }
 PromiseStream.prototype._end = function () {
 	if (this._state !== $STREAM_OPEN) {return}
-	if (this._processing === 0) {
+	if (this._process && this._processing === 0) {
 		this._state = $STREAM_CLOSED
 		this._cleanup()
 	} else {
@@ -83,17 +100,14 @@ PromiseStream.prototype._error = function (reason) {
 }
 PromiseStream.prototype._switchToIteratorMode = function (iterable) {
 	if (this._state !== $STREAM_OPEN) {return}
-	try {
-		if (Array.isArray(iterable)) {
-			this._queue = new ArrayIterator(iterable)
-		} else if (iterator && iterable != null && typeof iterable[iterator] === 'function') {
-			this._queue = iterable[iterator]()
-		} else {
-			throw new TypeError('Expected value to be an iterable object.')
+	if (Array.isArray(iterable)) {
+		this._queue = new ArrayIterator(iterable)
+	} else {
+		var it = getIterator(iterable)
+		if (it === IS_ERROR) {
+			return this._error(LAST_ERROR)
 		}
-	} catch (reason) {
-		this._error(reason)
-		return
+		this._queue = it
 	}
 	this._flush = _flushIterator
 	this._process && this._flush()
@@ -131,9 +145,7 @@ function MapProcess(source, dest, handler) {
 		if (source._state === $STREAM_CLOSED) {return}
 		--source._processing
 		source._flush()
-		if (source._state === $STREAM_CLOSING
-				&& source._processing === 0
-				&& (source._flush === _flushIterator || source._queue._length === 0)) {
+		if (source._state === $STREAM_CLOSING && source._processing === 0) {
 			source._state = $STREAM_CLOSED
 			source._cleanup()
 		}
@@ -143,6 +155,7 @@ function MapProcess(source, dest, handler) {
 		source._error(reason)
 	}
 	return function (data) {
+		// All processors must NOT decrement source._processing synchronously.
 		Promise.resolve(data)._then(handler)._then(onFulfilled, onRejected)
 	}
 }
@@ -155,6 +168,16 @@ function getNext(iterator) {
 	try {
 		var next = iterator.next()
 		return next.done ? IS_DONE : next.value
+	} catch (ex) {
+		LAST_ERROR = ex
+		return IS_ERROR
+	}
+}
+function getIterator(iterable) {
+	try {
+		if (iterator && iterable != null && typeof iterable[iterator] === 'function') {
+			return iterable[iterator]()
+		}
 	} catch (ex) {
 		LAST_ERROR = ex
 		return IS_ERROR
